23cef2c467957cf0b93fb35c3f57f0d5231b43f5
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-17 16:35:54 -0500

    database conexions. servername, hostname, password, etc.

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+/*
+since this file is purely php code it is no mandatory
+to write the closing tag ?>. The closing tag allows you to continue
+writing any type of language/code/html in the same file.
+*/
+$db_server_name
+$
