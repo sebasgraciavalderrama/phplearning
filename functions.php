39140e450c75c4ba7a6ffc94313a3f9834aefd77
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-16 20:14:38 -0500

    Final functions

--- functions.php
+++ functions.php
@@ -7,11 +7,13 @@
   </head>
   <body>
   <?php
-    $x = 100;
-      function newCalc(){
+
+      function newCalc($x){
         $newnr = $x * 0.75;
         echo "Here is 75% of what you wrote: ".$newnr;
       }
+      $x = 100;
+      newCalc($x);
    ?>
   </body>
 </html>
