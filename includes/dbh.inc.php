f9092fd52488efafe41dc0812f7a55a5bd6b8e47
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-17 16:50:21 -0500

    OOP elements added.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,9 +4,21 @@ since this file is purely php code it is no mandatory
 to write the closing tag ?>. The closing tag allows you to continue
 writing any type of language/code/html in the same file.
 */
-$db_server_name = "localhost"; //Since we are using XAMPP in our local machine (127.0.1), the following variables are setted the way they are. If you are connecting to a remote server, you must add the right parameters.
-$db_username = "root";
-$db_password = "";
-$db_name = "loginsystem";
+class dbh{
+  private $db_server_name;
+  private $db_username;
+  private $db_password;
+  private $db_name;
+
+  protected function connect(){
+    $this->db_server_name = "localhost";
+    $this->db_username = "root";
+    $this->db_password = "";
+    $this->db_name = "ooptutorial";
+
+    $conn = new mysqli();
+
+  }
+}
 
 $conn = mysqli_connect($db_server_name, $db_username, $db_password, $db_name);//string de conexion.
