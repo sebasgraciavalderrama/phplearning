d5892a923edaebef2d580838d875fc4d9d177fc0
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-16 21:01:22 -0500

    isset

---
+++ isset.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form method="get">
+      <input type="text" name="numero">
+      <input type="submit">
+    </form>
+    <?php
+
+
+
+
+     ?>
+
+  </body>
+</html>
