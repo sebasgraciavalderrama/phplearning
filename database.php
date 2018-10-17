59a46901f28a2f39e844b64255653a72690295ea
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-17 16:02:02 -0500

    sql added

--- database.php
+++ database.php
@@ -1,3 +1,13 @@
+<!--
+SQL CREATED IN PHPMYADMIN LOCALHOST XAMPP:
+CREATE TABLE posts (
+	id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    subject varchar(128) NOT NULL,
+    content varchar(1000) NOT NULL,
+    date datetime NOT NULL
+);
+
+-->
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
