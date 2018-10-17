920efa1d9857224c551070982d395f4c43683064
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-17 15:41:59 -0500

    Usage of isset method on a calculator

--- isset.php
+++ isset.php
@@ -6,14 +6,42 @@
   </head>
   <body>
     <form method="get">
-      <input type="text" name="numero">
-      <input type="submit">
+      <input type="text" name="num1" placeholder="Number 1">
+      <input type="text" name="num2" placeholder="Number 2">
+      <select name="operator">
+        <option>None</option>
+        <option>Add</option>
+        <option>Substract</option>
+        <option>Multiply</option>
+        <option>Divide</option>
+      </select>
+      <br>
+      <button type="submit" value="submit" name="submit">Calculate</button>
     </form>
+    <p>The answer is: </p>
     <?php
-
-
-
-
+      if (isset($_GET['submit'])) {
+        $result1 = $_GET['num1'];
+        $result2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+        switch ($operator) {
+          case "None":
+            echo "Error, you didn't select an operator";
+            break;
+          case "Add":
+          echo $result1+$result2;
+            break;
+          case "Substract":
+            echo $result1-$result2;
+            break;
+          case "Multiply":
+            echo $result1*$result2;
+            break;
+          case "Divide":
+            echo $result1/$result2;
+            break;
+        }
+      }
      ?>
 
   </body>
