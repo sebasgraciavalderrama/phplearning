83a391a6eed7b3efae6e21798f4891f115234db1
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2018-10-17 16:09:20 -0500

    insert sql added

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <!--
-SQL CREATED IN PHPMYADMIN LOCALHOST XAMPP:
+***SQL CREATED IN PHPMYADMIN LOCALHOST XAMPP:***
 CREATE TABLE posts (
 	id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     subject varchar(128) NOT NULL,
@@ -7,6 +7,8 @@ CREATE TABLE posts (
     date datetime NOT NULL
 );
 
+INSERT INTO posts (subject, content, date)
+VALUES ('This is the subject','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac porttitor nulla, eget volutpat enim. Mauris ac lacus pretium, posuere orci id, sodales odio. Maecenas euismod pulvinar diam, varius viverra magna elementum vel. Donec sollicitudin eros iaculis ex euismod tempor. Vivamus cursus nibh at nunc tincidunt faucibus. Quisque posuere viverra viverra. Phasellus ultrices arcu quis mauris convallis ultrices. Nam porttitor malesuada ultricies. Phasellus ultricies placerat quam non gravida.', '2018-10-17 16:08:05');
 -->
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
